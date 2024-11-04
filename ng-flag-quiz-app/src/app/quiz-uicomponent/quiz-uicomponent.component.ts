679c3a2845293a081c8a1716a865d8a2e11d6081
zmigliorini <zmigliorini@gmail.com>
2024-11-04 17:33:06 -0500

    initial setup

---
+++ ng-flag-quiz-app/src/app/quiz-uicomponent/quiz-uicomponent.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-quiz-uicomponent',
+  standalone: true,
+  imports: [],
+  templateUrl: './quiz-uicomponent.component.html',
+  styleUrl: './quiz-uicomponent.component.css'
+})
+export class QuizUIComponentComponent {
+
+}
